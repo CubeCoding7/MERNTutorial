eb6ca6a9d499f49fe7b183579c46b57f7d85ae84
CubeCoding <sljohnston714@gmail.com>
2024-07-29 15:20:04 -0400

    added a test app.get

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,6 +13,11 @@ const PORT = 5000;
 app.use(cors);
 app.use(express.json());
 
+app.get('/', async (req: Request, res: Response) => {
+    res.send('sup');
+    console.log('rendered')
+})
+
 app.post('/decks', async (req: Request, res: Response) => {
     
     const newDeck = new Deck({
