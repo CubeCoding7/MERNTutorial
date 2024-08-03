06dcccfdfbe999a9e3bf0617bd65a651e6c08bac
CubeCoding <sljohnston714@gmail.com>
2024-08-02 20:27:02 -0400

    added the delete functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,54 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 type TDeck = {
   title: string;
   _id: string;
-}
+};
 
 function App() {
   const [decks, setDecks] = useState<TDeck[]>([]);
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
 
   async function handleCreateDeck(e: React.FormEvent) {
     e.preventDefault();
-    await fetch('http://localhost:5000/decks', {
-      method: 'POST',
+    const response = await fetch("http://localhost:5000/decks", {
+      method: "POST",
       body: JSON.stringify({ title }),
       headers: {
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     });
+    const deck = await response.json();
+    setDecks([...decks, deck]);
     setTitle("");
+  }
+
+  async function handleDeleteDeck(deckId: string) {
+    await fetch(`http://localhost:5000/decks/${deckId}`, {
+      method: "DELETE",
+    });
+    setDecks(decks.filter((deck) => deck._id !== deckId));
   }
 
   useEffect(() => {
     (async () => {
-      const response = await fetch("http://localhost:5000/decks")
-      const newDecks = await response.json()
+      const response = await fetch("http://localhost:5000/decks");
+      const newDecks = await response.json();
       setDecks(newDecks);
-    })()
-  }, [])
+    })();
+  }, []);
 
   return (
     <div className="App">
       <ul className="decks">
-        {
-
-        decks.map((deck) => (
-          <li key={deck._id}>{deck.title}</li>
-        ))
-        }
+        {decks.map((deck) => (
+          <li key={deck._id}>
+            <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
+            <Link to={`decks/${deck._id}`}>{deck.title}</Link>
+          </li>
+        ))}
       </ul>
       <form onSubmit={handleCreateDeck}>
         <label htmlFor="deck-title">Deck Title</label>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,37 +1,44 @@
 import express, { Request, Response } from "express";
-import mongoose from 'mongoose';
-import cors from 'cors';
+import mongoose from "mongoose";
+import cors from "cors";
 
 import { config } from "dotenv";
 config();
 
-import Deck from './models/Deck'
+import Deck from "./models/Deck";
 
 const app = express();
 const PORT = 5000;
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
-app.get('/decks', async (req: Request, res: Response) => {
-    const decks = await Deck.find()
+app.get("/decks", async (req: Request, res: Response) => {
+  const decks = await Deck.find();
 
-    res.json(decks)
-})
-
-app.post('/decks', async (req: Request, res: Response) => {
-    
-    const newDeck = new Deck({
-        title: req.body.title
-    });
-    const createdDeck = await newDeck.save();
-    res.json(createdDeck);
+  res.json(decks);
 });
 
-mongoose.connect(process.env.MONGO_URI!).then(()=> {
-    console.log(`listening on port ${PORT}`)
-    app.listen(PORT)
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+});
+
+app.delete("/decks/:deckID", async (req: Request, res: Response) => {
+  const deckID = req.params.deckID;
+  const deck = await Deck.findByIdAndDelete(deckID);
+  res.json(deck);
+});
+
+mongoose.connect(process.env.MONGO_URI!).then(() => {
+  console.log(`listening on port ${PORT}`);
+  app.listen(PORT);
 });
