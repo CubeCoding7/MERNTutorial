733b8f046db033b8142ff9324d14d51b2dce982c
CubeCoding <sljohnston714@gmail.com>
2024-08-05 13:45:36 -0400

    added some error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import Deck from "./models/Deck";
 import { config } from "dotenv";
+import Deck from "./models/Deck";
 import { getDecksController } from "./controllers/getDecksController";
 import { createDeckController } from "./controllers/createDeckController";
 import { deleteDeckController } from "./controllers/deleteDeckController";
@@ -13,16 +13,23 @@ import { deleteCardController } from "./controllers/deleteCardController";
 config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
+// Middleware setup
 app.use(
   cors({
     origin: "*",
   })
 );
-
 app.use(express.json());
 
+// Logging middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`Received ${req.method} request for ${req.url}`);
+  next();
+});
+
+// Routes
 app.get("/decks", getDecksController);
 app.post("/decks", createDeckController);
 app.delete("/decks/:deckId", deleteDeckController);
@@ -30,7 +37,21 @@ app.get("/decks/:deckId", getDeckController);
 app.post("/decks/:deckId/cards", createCardController);
 app.delete("/decks/:deckId/cards/:index", deleteCardController);
 
-mongoose.connect(process.env.MONGO_URI!).then(() => {
-  console.log(`listening on port ${PORT}`);
-  app.listen(PORT);
+// Error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
 });
+
+// Database connection and server start
+mongoose
+  .connect(process.env.MONGO_URI!)
+  .then(() => {
+    console.log(`MongoDB connected`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
